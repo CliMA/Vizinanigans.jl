64927802d2af03beecfe050bbfcd79bce7e7cfb6
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-02-25 17:12:23 -0500

    Test examples

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,34 @@
+using Test
 using Vizinanigans
+
+function run_script(filepath)
+    try
+        include(filepath)
+    catch err
+        @warn "Error while testing script: " * sprint(showerror, err)
+
+        # Print the content of the file to the test log, with line numbers, for debugging
+        file_content = read(filepath, String)
+        delineated_file_content = split(file_content, '\n')
+        for (number, line) in enumerate(delineated_file_content)
+            @printf("% 3d %s\n", number, line)
+        end
+
+        return false
+    end
+    return true
+end
+
+@testset "Vizinanigans" begin
+    @testset "2D" begin
+        @test run_script(joinpath(@__FILE__, "..", "examples", "visualize_2D.jl"))
+    end
+
+    @testset "3D" begin
+        @test run_script(joinpath(@__FILE__, "..", "examples", "visualize_3D.jl"))
+    end
+
+    @testset "Volume slices" begin
+        @test run_script(joinpath(@__FILE__, "..", "examples", "volumeslice.jl"))
+    end
+end
